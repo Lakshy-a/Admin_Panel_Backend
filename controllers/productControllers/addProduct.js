89ee8b32fa73d9c64baa57333f780fe895ffcdcb
Lakshya_Gupta <lakshyagupta8786@gmail.com>
2024-08-02 14:41:27 +0530

    Commiting after adding the addProduct functionality to both backend and frontend

--- controllers/productControllers/addProduct.js
+++ controllers/productControllers/addProduct.js
@@ -3,13 +3,19 @@ const Product = require('../../models/productsModel');
 // addProduct route
 exports.addProduct = async (req, res) => {
   const { name, description, price, category, imageUrl } = req.body;
+
+  // Validate request body
+  if (!name || !description || !price || !category || !imageUrl) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+
   try {
     // Check if a product with the same name already exists
     const existingProduct = await Product.findOne({ name });
     if (existingProduct) {
       return res
         .status(400)
-        .json({ message: "Product with the same name already exists" });
+        .json({ message: 'Product with the same name already exists' });
     }
 
     // Create a new product
@@ -22,9 +28,9 @@ exports.addProduct = async (req, res) => {
     });
     await newProduct.save();
 
-    res.status(200).json({ message: "Product added successfully" });
+    res.status(200).json({ message: 'Product added successfully' });
   } catch (error) {
-    console.error("Error adding product:", error);
-    res.status(500).json({ message: "Internal server error" });
+    console.error('Error adding product:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
